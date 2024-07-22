e1849669ae669aec4c61d7338835b489db63da64
Cade <46201042+KinCadeFoster@users.noreply.github.com>
2024-07-23 00:39:01 +0400

    RA-03 - рефакторинг по замечаниям

--- app/src/main/java/ru/example/recipeapp/MainActivity.kt
+++ app/src/main/java/ru/example/recipeapp/MainActivity.kt
@@ -1,20 +1,18 @@
 package ru.example.recipeapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import ru.example.recipeapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
     }
 }
