f340b047d09aad82cfe515a331d0c90c587fa30c
Cade <46201042+KinCadeFoster@users.noreply.github.com>
2024-07-29 03:52:30 +0400

    RA-04 - имплементировал библиотеку fragment-ktx - создал CategoriesListFragment.kt и лейаут для него fragment_list_categories.xml - в разметке фрагмента fragment_list_categories.xml задал стандартный фон по макету и разместил TextView - В разметке основного лейаута activity_main.xml создал контейнер для фрагментов FragmentContainerView - В MainActivity.kt в методе onCreate() создал экземпляр FragmentTransaction

--- app/src/main/java/ru/example/recipeapp/MainActivity.kt
+++ app/src/main/java/ru/example/recipeapp/MainActivity.kt
@@ -2,17 +2,26 @@ package ru.example.recipeapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import ru.example.recipeapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
-    }
+        ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            val categoriesListFragment = CategoriesListFragment()
+            val fragmentManager: FragmentManager = supportFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.add(R.id.mainContainer, categoriesListFragment)
+            fragmentTransaction.commit()
+        }
     }
 }
