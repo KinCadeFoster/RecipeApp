50a8735d6d3d9012b46c2d520f52dda39b02cc69
Cade <46201042+KinCadeFoster@users.noreply.github.com>
2024-09-16 00:34:47 +0400

    RA-09 - создал RecipesListFragment - создал функцию обратного вызова, которая из адаптера передает клик во фрагмент - во фрагменте CategoriesListFragment реализовал навигацию на нужный переход на экран со списком рецептов выбранной категории

--- app/src/main/java/ru/example/recipeapp/CategoriesListFragment.kt
+++ app/src/main/java/ru/example/recipeapp/CategoriesListFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import ru.example.recipeapp.databinding.FragmentListCategoriesBinding
+import ru.example.recipeapp.model.Category
 import ru.example.recipeapp.model.STUB
 
 
@@ -30,6 +33,21 @@ class CategoriesListFragment : Fragment() {
     private fun initRecycler() {
         val categoriesAdapter = CategoriesListAdapter(STUB.getCategories())
         binding.rvCategories.adapter = categoriesAdapter
+
+        categoriesAdapter.setOnItemClickListener(object :
+            CategoriesListAdapter.OnItemClickListener {
+            override fun onItemClick(category: Category) {
+                openRecipesByCategoryId(category.id)
+            }
+        })
+    }
+
+    private fun openRecipesByCategoryId(categoryId: Int) {
+        parentFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace<RecipesListFragment>(R.id.mainContainer)
+            addToBackStack(null)
+        }
     }
 
 }
