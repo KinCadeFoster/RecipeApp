8b8cfc96fd78f6073f4f8f0fcd3f21739ec11bbe
Cade <46201042+KinCadeFoster@users.noreply.github.com>
2024-10-11 11:12:54 +0400

    RA-11 - правки по замечаниям

--- app/src/main/java/ru/example/recipeapp/RecipeFragment.kt
+++ app/src/main/java/ru/example/recipeapp/RecipeFragment.kt
@@ -6,16 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import ru.example.recipeapp.databinding.FragmentRecipeBinding
-import ru.example.recipeapp.model.Ingredient
 
 
 class RecipeFragment : Fragment() {
-    private var recipeId: Int? = null
-    private var recipeName: String? = null
-    private var recipeImageUrl: String? = null
-    private var recipeMethod: List<String>? = null
-    private var recipeIngredients: List<Ingredient>? = null
-
     private val binding by lazy {
         FragmentRecipeBinding.inflate(layoutInflater)
     }
@@ -29,14 +22,6 @@ class RecipeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        recipeId = arguments?.getInt("ARG_RECIPE_ID")
-        recipeName = arguments?.getString("ARG_RECIPE_NAME")
-
-        binding.textView1.text = recipeId.toString()
-        binding.textView2.text = recipeName
-
+        binding.textView1.text = "Рецепт"
     }
-
-
 }
--- app/src/main/java/ru/example/recipeapp/RecipesListFragment.kt
+++ app/src/main/java/ru/example/recipeapp/RecipesListFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import ru.example.recipeapp.databinding.FragmentListRecipesBinding
@@ -60,24 +59,10 @@ class RecipesListFragment : Fragment() {
     }
 
     private fun openRecipeByRecipeId(recipeId: Int) {
-        STUB.getRecipesByCategoryId(categoryId!!).find { it.id == recipeId }?.let { recipe ->
-            val recipeName = recipe.title
-            val recipeImageUrl = recipe.imageUrl
-            val ingredients = recipe.ingredients
-            val method = recipe.method
-
-            val bundle = bundleOf(
-                Constants.ARG_RECIPE_ID to recipe.id,
-                Constants.ARG_RECIPE_NAME to recipeName,
-                Constants.ARG_RECIPE_IMAGE_URL to recipeImageUrl,
-                Constants.ARG_RECIPE_INGREDIENTS to ingredients,
-                Constants.ARG_RECIPE_METHOD to method
-            )
             parentFragmentManager.commit {
                 setReorderingAllowed(true)
-                replace<RecipeFragment>(R.id.mainContainer, args = bundle)
+                replace<RecipeFragment>(R.id.mainContainer)
                 addToBackStack(null)
             }
         }
-    }
 }
