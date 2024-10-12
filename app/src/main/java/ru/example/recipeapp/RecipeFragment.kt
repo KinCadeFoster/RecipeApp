0b67d2fc3ac930a6a72a171f1b107454e56e4b9f
Cade <46201042+KinCadeFoster@users.noreply.github.com>
2024-10-12 20:18:32 +0400

    RA-12 - Имплементировал плагин kotlin-parcelize - Добавил аннотации @Parcelize - Добавил метод getRecipeById() - Доработал фрагмент - Вывел на экране текст объекта рецепта

--- app/src/main/java/ru/example/recipeapp/RecipeFragment.kt
+++ app/src/main/java/ru/example/recipeapp/RecipeFragment.kt
@@ -1,11 +1,14 @@
 package ru.example.recipeapp
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import ru.example.recipeapp.databinding.FragmentRecipeBinding
+import ru.example.recipeapp.model.Recipe
 
 
 class RecipeFragment : Fragment() {
@@ -22,6 +25,24 @@ class RecipeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.textView1.text = "Рецепт"
+
+        val recipe = getRecipeFromArguments()
+
+        if (recipe != null) {
+            binding.textView1.text = recipe.title
+        } else {
+            Toast.makeText(requireContext(), "Не нашли такого рецепта", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun getRecipeFromArguments(): Recipe? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            arguments?.getParcelable(Constants.ARG_RECIPE, Recipe::class.java)
+        } else {
+            @Suppress("DEPRECATION")
+            arguments?.getParcelable(Constants.ARG_RECIPE)
+        }
     }
 }
+
+
--- app/src/main/java/ru/example/recipeapp/model/Ingredient.kt
+++ app/src/main/java/ru/example/recipeapp/model/Ingredient.kt
@@ -1,7 +1,11 @@
 package ru.example.recipeapp.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Ingredient(
     val quantity: String,
     val unitOfMeasure: String,
     val description: String
-)
+) : Parcelable
