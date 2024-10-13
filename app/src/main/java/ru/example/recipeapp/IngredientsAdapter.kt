e594fcf005dd960d8021a7c17c2d3d7ee8b2a8ef
Cade <46201042+KinCadeFoster@users.noreply.github.com>
2024-10-13 20:33:22 +0400

    RA-13 - Добавил тэг NestedScrollView в качестве основного контейнера - Реализовал в шапке изображение и название рецепта, стилизовал согласно макету - Добавил заголовки для секций 'Ингредиенты' и 'Способ приготовления' - Добавил два контейнера RecyclerView для соответствующих списков (rvIngredients и rvMethod) - Создал адаптер для ингредиентов IngredientsAdapter и шагов приготовления MethodAdapter - Реализовал горизонтальный разделитель между элементами списков - Оптимизировал код

---
+++ app/src/main/java/ru/example/recipeapp/IngredientsAdapter.kt
@@ -0,0 +1,27 @@
+package ru.example.recipeapp
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.example.recipeapp.databinding.ItemIngredientBinding
+import ru.example.recipeapp.model.Ingredient
+
+class IngredientsAdapter(private val dataSet: List<Ingredient>) :
+    RecyclerView.Adapter<IngredientsAdapter.ViewHolder>() {
+
+    class ViewHolder(val binding: ItemIngredientBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            ItemIngredientBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val ingredient: Ingredient = dataSet[position]
+        holder.binding.tvIngredient.text = ingredient.description
+    }
+
+    override fun getItemCount() = dataSet.size
+}
--- app/src/main/java/ru/example/recipeapp/RecipeFragment.kt
+++ app/src/main/java/ru/example/recipeapp/RecipeFragment.kt
@@ -1,5 +1,6 @@
 package ru.example.recipeapp
 
+import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.divider.MaterialDividerItemDecoration
 import ru.example.recipeapp.databinding.FragmentRecipeBinding
 import ru.example.recipeapp.model.Recipe
 
@@ -29,9 +32,14 @@ class RecipeFragment : Fragment() {
         val recipe = getRecipeFromArguments()
 
         if (recipe != null) {
-            binding.tVRecipeTitle.text = recipe.title
+            initUI(recipe)
+            initRecycler(recipe)
         } else {
-            Toast.makeText(requireContext(), getString(R.string.recipe_not_found), Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                requireContext(),
+                getString(R.string.recipe_not_found),
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
@@ -43,6 +51,40 @@ class RecipeFragment : Fragment() {
             arguments?.getParcelable(Constants.ARG_RECIPE)
         }
     }
+
+    private fun initUI(recipe: Recipe) {
+        binding.tVHeader.text = recipe.title
+
+        val recipeUrl = recipe.imageUrl
+        recipeUrl.let {
+            val imageResources = requireContext().assets.open(it).use { inputStream ->
+                BitmapFactory.decodeStream(inputStream)
+            }
+            binding.headerImage.setImageBitmap(imageResources)
+        }
+    }
+
+    private fun initRecycler(recipe: Recipe) {
+        binding.rvIngredients.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvMethod.layoutManager = LinearLayoutManager(requireContext())
+
+        binding.rvIngredients.adapter = IngredientsAdapter(recipe.ingredients)
+        binding.rvMethod.adapter = MethodAdapter(recipe.method)
+
+        val ingredientDivider = createDivider()
+        val methodDivider = createDivider()
+
+        binding.rvIngredients.addItemDecoration(ingredientDivider)
+        binding.rvMethod.addItemDecoration(methodDivider)
+    }
+
+    private fun createDivider(): MaterialDividerItemDecoration {
+        return MaterialDividerItemDecoration(
+            requireContext(),
+            LinearLayoutManager.VERTICAL
+        ).apply {
+            dividerThickness = resources.getDimensionPixelSize(R.dimen.divider_thickness)
+            dividerColor = resources.getColor(R.color.divider_color, null)
+        }
+    }
 }
-
-
