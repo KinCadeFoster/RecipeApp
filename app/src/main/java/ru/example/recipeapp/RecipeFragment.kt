1072bfe00084e416a2ac94b79eba7cc71701b9b2
Cade <46201042+KinCadeFoster@users.noreply.github.com>
2024-10-19 21:35:57 +0400

    RA-16 - Экспортировал иконки - Сверстал кнопку - Добавил слушатель - добавил круговой риппл эффект при нажатии на кнопку

--- app/src/main/java/ru/example/recipeapp/RecipeFragment.kt
+++ app/src/main/java/ru/example/recipeapp/RecipeFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.SeekBar
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,6 +20,9 @@ class RecipeFragment : Fragment() {
     private val binding by lazy {
         FragmentRecipeBinding.inflate(layoutInflater)
     }
+
+    private lateinit var imageHeartButton: ImageButton
+    private var isFavorite: Boolean = false
 
     private lateinit var ingredientsAdapter: IngredientsAdapter
 
@@ -69,7 +73,18 @@ class RecipeFragment : Fragment() {
                 R.string.content_description_recipe_image, recipe.title
             )
         }
+
+        imageHeartButton = binding.imageHeartButton
+        imageHeartButton.setOnClickListener {
+            isFavorite = !isFavorite
+            if (isFavorite) {
+                imageHeartButton.setImageResource(R.drawable.ic_heart)
+            } else {
+                imageHeartButton.setImageResource(R.drawable.ic_heart_empty)
+            }
+        }
     }
+
 
     private fun initRecycler(recipe: Recipe) {
         ingredientsAdapter = IngredientsAdapter(recipe.ingredients)
